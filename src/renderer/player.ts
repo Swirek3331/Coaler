1cbaad59e0965eee040e7a9f2d2374bcf6cfd90d
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-11-06 17:43:28 +0100

    Skupmy się może na czystej strukturze

--- src/renderer/player.ts
+++ src/renderer/player.ts
@@ -1,23 +1,3 @@
 let coal: Number = 0;
 let money: Number = 0;
 let companyName: string = "Coal Inc.";
-
-let coalDisplay = document.getElementById("coal-display");
-let moneyDisplay = document.getElementById("money-display");
-let companyNameDisplay = document.getElementById("company-name-display");
-
-function statsUpdate() :void
-{
-    if (coalDisplay)
-    {
-        coalDisplay.innerHTML = "Węgiel: " + coal.toString();
-    }
-    if (moneyDisplay)
-    {
-        moneyDisplay.innerHTML = "Fundusze: " + money.toString();
-    }
-    if (companyNameDisplay)
-    {
-        companyNameDisplay.innerHTML = companyName;
-    }    
-}
