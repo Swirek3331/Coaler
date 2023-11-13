a4fec06a3e78b2e08935b6db5276f4b1e59774f5
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-11-13 13:48:35 +0100

    guzik

--- dist/renderer/player.js
+++ dist/renderer/player.js
@@ -2,17 +2,10 @@
 let coal = 0;
 let money = 0;
 let companyName = "Coal Inc.";
-let coalDisplay = document.getElementById("coal-display");
-let moneyDisplay = document.getElementById("money-display");
-let companyNameDisplay = document.getElementById("company-name-display");
-function statsUpdate() {
-    if (coalDisplay) {
-        coalDisplay.innerText = "Węgiel: " + coal.toString();
-    }
-    if (moneyDisplay) {
-        moneyDisplay.innerHTML = "Fundusze: " + money.toString();
-    }
-    if (companyNameDisplay) {
-        companyNameDisplay.innerHTML = companyName;
-    }
+let nameLabel = document.getElementById("company-name-display");
+let coalLabel = document.getElementById("coal-display");
+let moneyLabel = document.getElementById("money-label");
+function click() {
+    coal++;
+    console.log(coal);
 }
--- src/renderer/player.ts
+++ src/renderer/player.ts
@@ -1,3 +1,14 @@
-let coal: Number = 0;
-let money: Number = 0;
-let companyName: string = "Coal Inc.";
+let coal: number = 0
+let money: number = 0
+let companyName: string = "Coal Inc."
+
+let nameLabel = document.getElementById("company-name-display")
+let coalLabel = document.getElementById("coal-display")
+let moneyLabel = document.getElementById("money-label")
+
+function click() :void
+{
+    coal++;
+
+    console.log(coal)
+}
