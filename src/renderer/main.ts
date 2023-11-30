01e7c01f90daeb85333b978cef570b0a443a66b3
Karol Sutkowski <karolsutkowski21@gmail.com>
2023-11-30 18:00:04 +0100

    Już

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,1 +1,17 @@
 updateLabels()
+
+function updateLabels() :void
+{
+    if (nameLabel)
+    {
+        nameLabel.innerHTML = companyName;
+    }
+    if (coalLabel)
+    {
+        coalLabel.innerHTML = "Węgiel: " + coal;
+    }
+    if (moneyLabel)
+    {
+        moneyLabel.innerHTML = "Fundusze: " + money;
+    }
+}
--- src/renderer/player.ts
+++ src/renderer/player.ts
@@ -12,19 +12,3 @@ function coalClick() :void
 
     updateLabels();
 }
-
-function updateLabels() :void
-{
-    if (nameLabel)
-    {
-        nameLabel.innerHTML = companyName;
-    }
-    if (coalLabel)
-    {
-        coalLabel.innerHTML = "Węgiel: " + coal;
-    }
-    if (moneyLabel)
-    {
-        moneyLabel.innerHTML = "Fundusze: " + money;
-    }
-}
