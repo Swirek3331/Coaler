f1c6e96c913d81d2b8448fc6e2db55551ca653f9
Karol Sutkowski <karolsutkowski21@gmail.com>
2024-03-19 19:12:58 +0100

    Ja się załamię

--- dist/renderer/backgrounds.js
+++ dist/renderer/backgrounds.js
@@ -2,11 +2,13 @@
 const backgroundKey = "bg_middle0";
 const backgroundAmount = 5;
 let currentBackground = 0;
+let previousBackground = 0;
 function nextBackground() {
     let next = randomInt(1, backgroundAmount);
-    while (next == currentBackground) {
+    while (next == currentBackground || next == previousBackground) {
         next = randomInt(1, backgroundAmount);
     }
+    previousBackground = currentBackground;
     currentBackground = next;
     body.style.backgroundImage = `url(../assets/sprites/backgrounds/scalled/${backgroundKey}${currentBackground}.png)`;
 }
