1d99e18bec3dda5a745dbb005446225786319a56
Karol Sutkowski <karolsutkowski21@gmail.com>
2024-03-19 19:16:04 +0100

    A czemu jak go trzymałem

--- dist/renderer/player.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let coalAmount = 1;
-let money = 0;
-let companyName = "Coal Inc.";
-let currentTool;
-let currentCoal;
-let cheatsEnabled = false;
-let devMode = false;
-let coalShow = false;
--- dist/renderer/start.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-coalButton.addEventListener("click", currentCoal.click);
-ShopItems.init();
-CoalItem.init();
-setInterval(nextBackground, 10 * 1000);
-updateLabels();
