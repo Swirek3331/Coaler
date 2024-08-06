3a908e3ba4f9e80b296f73d21105479fbf1ba74e
Karol Sutkowski <karolsutkowski21@gmail.com>
2024-08-06 17:53:27 +0200

    Japierdole, to się klika

---
+++ gamec/Assets/Src/Menager.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Menager : MonoBehaviour
+{
+    public TextMeshProUGUI coalLabel;
+
+    public int coalAmount = 0;
+
+    public void AddCoal()
+    {
+        coalAmount++;
+        Debug.Log("Coal amount: " + coalAmount);
+        coalLabel.text = coalAmount.ToString();
+    }
+}
