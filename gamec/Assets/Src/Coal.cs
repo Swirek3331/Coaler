01423c81ffbffa55b0383b144ca1b739793bcaeb
Karol Sutkowski <karolsutkowski21@gmail.com>
2024-08-08 19:26:34 +0200

    koooparki

---
+++ gamec/Assets/Src/Coal.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+/*public class Coal
+{
+    int amount = 0;
+
+    readonly string name;
+    readonly string path;
+    readonly string scalledPath;
+
+    public Coal(string name)
+    {
+        this.name = name;
+    }
+}
+*/
--- gamec/Assets/Src/Menager.cs
+++ gamec/Assets/Src/Menager.cs
@@ -2,6 +2,8 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEditor.SearchService;
+using System.ComponentModel;
 
 public class Menager : MonoBehaviour
 {
@@ -9,11 +11,32 @@ public class Menager : MonoBehaviour
 
     public int coalAmount = 0;
 
+    int excavatorAmount = 0;
+
     public void AddCoal()
     {
         coalAmount++;
-        Debug.Log("Coal amount: " + coalAmount);
+        //Debug.Log("Coal amount: " + coalAmount);
         coalLabel.text = coalAmount.ToString();
+    }
+
+    public void AddExcavator()
+    {
+        GameObject excavator = new GameObject("Excavator");
+
+        SpriteRenderer spriteRenderer = excavator.AddComponent<SpriteRenderer>();
+        spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/excavator");
+
+        float angle = UnityEngine.Random.Range(0, 360);
+        Debug.Log(angle);
+
+        float angleInRadians = angle * Mathf.Deg2Rad;
+
+        excavator.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        excavator.transform.position = new Vector3(Mathf.Cos(angleInRadians) * 2, Mathf.Sin(angleInRadians) * 2, 0);
+
+        excavatorAmount++;
     }
 
     public void Update()
@@ -22,5 +45,13 @@ public class Menager : MonoBehaviour
         {
             AddCoal();
         }
+
+        coalAmount += excavatorAmount;
+        coalLabel.text = coalAmount.ToString();
+    }
+
+    public void Start()
+    {
+        coalLabel.text = coalAmount.ToString();
     }
 }
